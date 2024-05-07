60e313b5d720e32bdaf5031a7318e1eb3bd1a34b
Taras Kor <tkordevmail@gmail.com>
2024-05-07 21:36:44 +0300

    get path to folder

--- routes/folders/folder.routes.ts
+++ routes/folders/folder.routes.ts
@@ -1,22 +1,55 @@
 import { Hono } from "hono";
-import { getFolderByid } from "../../controllers/folders/folder.controllers";
+import {
+  createFolder,
+  getAllFolders,
+  getAllParents,
+  getFolderByid,
+  getFolderChildren,
+} from "../../controllers/folders/folder.controllers";
 
 const folders = new Hono();
 
 folders
-  .get("/", (c) => {
-    return c.json({ message: "GET ALL FOLDERS" });
+  .get("/", async (c) => {
+    const folders = await getAllFolders();
+    if (!folders) {
+      return c.json({ message: "ERROR GETTING FOLDERS" }, 500);
+    }
+    return c.json({ folders });
   })
   .get("/:id", async (c) => {
     const { id } = c.req.param();
     const folder = await getFolderByid(id);
-    console.log(folder);
-    return c.json({ message: `GET FOLDER ${id}` });
+    if (!folder) {
+      return c.json({ message: "FOLDER NOT FOUND" }, 404);
+    }
+    const parents = await getAllParents(folder.id);
+    if (!parents) {
+      return c.json({ message: "ERROR GETTING PARENTS" }, 500);
+    }
+    const folderChildren = await getFolderChildren(folder.id);
+    if (!folderChildren) {
+      return c.json({ message: "ERROR GETTING CHILDREN" }, 500);
+    }
+    return c.json({
+      folder: id,
+      parents,
+      children: {
+        folders: folderChildren.children_folders,
+        files: folderChildren.children_files,
+      },
+    });
   })
   .post("/", async (c) => {
-    const body = await c.req.json();
-    console.log(body);
-    return c.json({ message: "POST FOLDER" });
+    const body = (await c.req.json()) as {
+      name: string;
+      parent_folder_id: string;
+    };
+    const createdFolder = await createFolder(body.name, body.parent_folder_id);
+    if (!createdFolder) {
+      return c.json({ message: "ERROR CREATING FOLDER" }, 500);
+    }
+    return c.json({ folder: createdFolder });
   });
 
 export default folders;
